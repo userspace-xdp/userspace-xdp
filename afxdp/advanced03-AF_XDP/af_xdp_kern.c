668a3aa1015cca8d96cbf8cc595b57b970e3450a
yunwei37 <yunwei356@gmail.com>
2024-02-05 10:05:52 +0000

    add afxdp

---
+++ afxdp/advanced03-AF_XDP/af_xdp_kern.c
@@ -0,0 +1,43 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#include <linux/bpf.h>
+
+#include <bpf/bpf_helpers.h>
+
+struct {
+	__uint(type, BPF_MAP_TYPE_XSKMAP);
+	__type(key, __u32);
+	__type(value, __u32);
+	__uint(max_entries, 64);
+} xsks_map SEC(".maps");
+
+struct {
+	__uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
+	__type(key, __u32);
+	__type(value, __u32);
+	__uint(max_entries, 64);
+} xdp_stats_map SEC(".maps");
+
+SEC("xdp")
+int xdp_sock_prog(struct xdp_md *ctx)
+{
+    int index = ctx->rx_queue_index;
+    __u32 *pkt_count;
+
+    pkt_count = bpf_map_lookup_elem(&xdp_stats_map, &index);
+    if (pkt_count) {
+
+        /* We pass every other packet */
+        if ((*pkt_count)++ & 1)
+            return XDP_PASS;
+    }
+
+    /* A set entry here means that the correspnding queue_id
+     * has an active AF_XDP socket bound to it. */
+    if (bpf_map_lookup_elem(&xsks_map, &index))
+        return bpf_redirect_map(&xsks_map, index, 0);
+
+    return XDP_PASS;
+}
+
+char _license[] SEC("license") = "GPL";
