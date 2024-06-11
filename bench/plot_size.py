d2732277d736a51e27d44f1ad9049ad1d44d5e5e
yunwei37 <yunwei356@gmail.com>
2024-06-11 08:15:00 +0000

    add new figture

--- bench/plot_size.py
+++ bench/plot_size.py
@@ -58,6 +58,9 @@ def plot_data(data, dirname, title='Data Plot', xlabel='X-axis', ylabel='Y-axis'
     plt.tight_layout()  # Adjust layout to not cut off labels
     plt.savefig(dirname + '/ipackets.png')
 
+all_data = {}
+plot_list = ["skb_mode", "drv_mode", "dpdk_llvm", "afxdp_llvm_zero_copy", "afxdp_llvm_copy"]
+
 def collect_data(root_dir):
     """
     Traverse the directory, collect and aggregate data from target files.
@@ -68,7 +71,34 @@ def collect_data(root_dir):
             dir_full_path = os.path.join(dirpath, dirname)
             print(f"Processing directory: {dir_full_path}")
             data = collect_data_single_directory(dir_full_path)
+            if dirname in plot_list:
+                all_data[dirname] = data
             plot_data(data, dir_full_path, title='Ipackets by Packet Size in ' + root_dir + ' ' + name,
               xlabel='Packet Size (Bytes)', ylabel='Pkt/s')
 
 collect_data(root_dir)
+print(all_data)
+
+def plot_all_data(data):
+    """
+    Plots the provided data in a line plot.
+    
+    Parameters:
+    - data (dict): A dictionary where keys are dataset names and values are dictionaries of data points.
+    """
+    labels = ['64', '128', '256', '512', '1024']
+
+    plt.figure(figsize=(7, 5))
+
+    for key, values in data.items():
+        # Ensure that values are in the correct order based on labels
+        sorted_values = [values.get(label, None) for label in labels]
+        plt.plot(labels, sorted_values, marker='o', label=key)
+
+    plt.xlabel('Pkt size', fontsize=18)
+    plt.ylabel('Pkt/s', fontsize=18)
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(name + '/line.png')
+    
+plot_all_data(all_data)
